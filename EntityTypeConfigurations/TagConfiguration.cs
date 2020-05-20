f5e60d87a261956f05d90c3018d10ae9e9ca2edc
Szymon098 <szymon.sliwinski@interia.pl>
2020-05-20 18:23:56 +0200

    First commit

---
+++ EntityTypeConfigurations/TagConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Vidzy.Models;
+
+namespace Vidzy.EntityTypeConfigurations
+{
+    public class TagConfiguration : EntityTypeConfiguration<Tag>
+    {
+        public TagConfiguration()
+        {
+            Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+}
---
+++ Models/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidzy.Models
+{
+    public class Tag
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Video> Videos { get; private set; }
+
+        public Tag()
+        {
+            Videos = new HashSet<Video>();
+        }
+    }
+}
